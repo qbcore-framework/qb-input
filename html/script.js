9ee74650add620543d6101b612738c33f8361b3f
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-02-26 17:30:27 -0500

    [Update] Default Value Support + Hotfixes
    - Added support for default values for all input types
    - Added all default value options to README
    - Added password type example to README
    - Fixed some CSS issues with select box

--- html/script.js
+++ html/script.js
@@ -10,9 +10,7 @@ const OpenMenu = (data) => {
 
     let form = [
         "<form id='qb-input-form'>",
-        `<div class="heading">${
-            data.header != null ? data.header : "Form Title"
-        }</div>`,
+        `<div class="heading">${ data.header != null ? data.header : "Form Title" }</div>`,
     ];
 
     data.inputs.forEach((item, index) => {
@@ -40,9 +38,7 @@ const OpenMenu = (data) => {
         }
     });
     form.push(
-        `<div class="footer"><button type="submit" class="btn btn-success" id="submit">${
-            data.submitText ? data.submitText : "Submit"
-        }</button></div>`
+        `<div class="footer"><button type="submit" class="btn btn-success" id="submit">${data.submitText ? data.submitText : "Submit"}</button></div>`
     );
 
     form.push("</form>");
@@ -72,28 +68,28 @@ const OpenMenu = (data) => {
 
 const renderTextInput = (item) => {
     const { text, name } = item;
-    formInputs[name] = "";
-    const isRequired =
-        item.isRequired == "true" || item.isRequired ? "required" : "";
+    formInputs[name] = item.default ? item.default : "";
+    const isRequired = item.isRequired == "true" || item.isRequired ? "required" : "";
+    const defaultValue = item.default ? `value="${item.default}"` : ""
 
-    return ` <input placeholder="${text}" type="text" class="form-control" name="${name}" ${isRequired}/>`;
+    return ` <input placeholder="${text}" type="text" class="form-control" name="${name}" ${defaultValue} ${isRequired}/>`;
 };
 const renderPasswordInput = (item) => {
     const { text, name } = item;
-    formInputs[name] = "";
-    const isRequired =
-        item.isRequired == "true" || item.isRequired ? "required" : "";
+    formInputs[name] = item.default ? item.default : "";
+    const isRequired = item.isRequired == "true" || item.isRequired ? "required" : "";
+    const defaultValue = item.default ? `value="${item.default}"` : ""
 
-    return ` <input placeholder="${text}" type="password" class="form-control" name="${name}" ${isRequired}/>`;
+    return ` <input placeholder="${text}" type="password" class="form-control" name="${name}" ${defaultValue} ${isRequired}/>`;
 };
 const renderNumberInput = (item) => {
     try {
         const { text, name } = item;
-        formInputs[name] = "";
-        const isRequired =
-            item.isRequired == "true" || item.isRequired ? "required" : "";
+        formInputs[name] = item.default ? item.default : "";
+        const isRequired = item.isRequired == "true" || item.isRequired ? "required" : "";
+        const defaultValue = item.default ? `value="${item.default}"` : ""
 
-        return `<input placeholder="${text}" type="number" class="form-control" name="${name}" ${isRequired}/>`;
+        return `<input placeholder="${text}" type="number" class="form-control" name="${name}" ${defaultValue} ${isRequired}/>`;
     } catch (err) {
         console.log(err);
         return "";
@@ -107,9 +103,8 @@ const renderRadioInput = (item) => {
     let div = `<div class="form-input-group"> <div class="form-group-title">${text}</div>`;
     div += '<div class="input-group">';
     options.forEach((option, index) => {
-        div += `<label for="radio_${name}_${index}"> <input type="radio" id="radio_${name}_${index}" name="${name}" value="${
-            option.value
-        }" ${index == 0 ? "checked" : ""}> ${option.text}</label>`;
+        div += `<label for="radio_${name}_${index}"> <input type="radio" id="radio_${name}_${index}" name="${name}" value="${option.value}" 
+                ${(item.default ? item.default == option.value : index == 0) ? "checked" : ""}> ${option.text}</label>`;
     });
 
     div += "</div>";
@@ -125,8 +120,9 @@ const renderCheckboxInput = (item) => {
     div += '<div class="input-group-chk">';
 
     options.forEach((option, index) => {
-        div += `<label for="chk_${name}_${index}">${option.text} <input type="checkbox" id="chk_${name}_${index}" name="${name}" value="${option.value}"></label>`;
-        formInputs[option.value] = 'false';
+        console.log(option)
+        div += `<label for="chk_${name}_${index}">${option.text} <input type="checkbox" id="chk_${name}_${index}" name="${name}" value="${option.value}" ${option.checked ? 'checked' : ''}></label>`;
+        formInputs[option.value] = option.checked ? 'true' : 'false';
     });
 
     div += "</div>";
@@ -142,9 +138,9 @@ const renderSelectInput = (item) => {
     formInputs[name] = options[0].value;
 
     options.forEach((option, index) => {
-        div += `<option value="${option.value}" ${
-            option.checked != null ? "checked" : ""
-        }>${option.text}</option>`;
+        const isDefaultValue = item.default == option.value
+        div += `<option value="${option.value}" ${isDefaultValue ? 'selected' : '' }>${option.text}</option>`;
+        if(isDefaultValue){ formInputs[name] = option.value }
     });
     div += "</select>";
     return div;
